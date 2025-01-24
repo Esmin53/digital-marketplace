5ee3c13db39f3e22764447361a09d2465e352927
Esmin53 <esmin.tufekcic53@gmail.com>
2025-01-24 23:27:13 +0100

    Add product detail page

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -1,18 +1,20 @@
 import { useState } from "react";
 import { FaRegStar, FaStar } from "react-icons/fa6";
+import { useNavigate } from "react-router-dom";
 
 const Card = () => {
   const [isFav, setIsFav] = useState<boolean >(false)
+  const navigate = useNavigate();
 
   return (
-    <div className='max-w-80 w-full overflow-hidden font-rubik relative flex flex-col cursor-pointer'>
+    <div className='max-w-80 w-full overflow-hidden font-rubik relative flex flex-col'>
         <div className="absolute top-2 right-2 text-accent-teal text-2xl font-semibold cursor-pointer z-40" onClick={() => setIsFav(prev => !prev)}>
           {isFav ? <FaStar  /> : <FaRegStar  />}
         </div>
         <div className="w-full aspect-square relative rounded-xl overflow-hidden shadow ">
             <img src="https://img.freepik.com/free-vector/white-100-universal-web-icons-set_1057-1119.jpg?t=st=1737605118~exp=1737608718~hmac=49d95bcf0d0f2462e0ad212cf5738b8c3ff1158c6a6ad9e85712f6792bbf8cfd&w=740" className="w-full h-full object-cover"/>
         </div>
-        <div className=" w-full px-2 py-1 text-[#1d2529] flex flex-col">
+        <div className=" w-full px-2 py-1 text-[#1d2529] flex flex-col cursor-pointer" onClick={() => navigate('/products/slug')}>
             <h1 className="text-lg">Item Title</h1>
             <p className="text-accent-gray">USD <span>4.75</span></p>
             <div className="flex items-center justify-between w-full flex-wrap">
